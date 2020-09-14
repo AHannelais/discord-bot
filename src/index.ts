01175501726dd415ea7c3880448461dbc5841df3
AHANNELAIS <antoine.hannelais@gmail.com>
2020-09-14 19:18:49 +0200

    ready event

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,11 @@ import { Client } from 'discord.js';
 dotenv.config();
 console.log(process.env.DISCORDJS_JS_BOT_TOKEN);
 const client = new Client();
+client.on('ready', () => {
+    console.log(`${client.user?.username} has logged in`);
+});
+client.on('message', (message) => {
+    console.log(`${client.user?.username} has logged in`);
+});
+
 client.login(process.env.DISCORDJS_JS_BOT_TOKEN);
