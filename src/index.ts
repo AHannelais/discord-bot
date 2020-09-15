0498dce1c3c93b7517802846eaee57d9ec7b6c03
AHANNELAIS <antoine.hannelais@gmail.com>
2020-09-15 06:49:26 +0200

    kick method

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,29 @@ import * as dotenv from 'dotenv';
 import { Client } from 'discord.js';
 
 dotenv.config();
+let jews: string[] = [];
 console.log(process.env.DISCORDJS_JS_BOT_TOKEN);
 const client = new Client();
+const PREFIX = '/';
 client.on('ready', () => {
     console.log(`${client.user?.username} has logged in`);
 });
 client.on('message', (message) => {
-    console.log(`${client.user?.username} has logged in`);
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+        console.log(CMD_NAME);
+        console.log(args);
+        if (CMD_NAME === 'kick') {
+            if (args.length === 0) return message.reply('please prodived an id');
+            const member = message.guild?.members.cache.get(args[0]);
+            if (member) {
+                member.kick();
+            } else {
+                message.channel.send('member not found');
+            }
+        }
+    }
 });
 
 client.login(process.env.DISCORDJS_JS_BOT_TOKEN);
